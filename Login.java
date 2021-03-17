a718ff54ff485e2d58a2dc4050f934447284beda
FelicitySmoak1101 <42353284+FelicitySmoak1101@users.noreply.github.com>
2021-03-17 00:03:18 -0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,34 +1,53 @@
 package com.example.aula;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-    private Button buttonLogin;
+    private EditText edtNome;
+    private EditText edtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        edtSenha=(EditText) findViewById(R.id.editTextTextPassword);
+        edtNome=(EditText) findViewById(R.id.editTextTextPersonName);
         Button buttonLogin = (Button) findViewById(R.id.btnLogin);
-        buttonLogin.setOnClickListener(this);
+        buttonLogin.setOnClickListener(this::onClickLogin);
     }
 
-    @Override
+
     public void onClickLogin (View view){
-        Intent intent = new Intent(this, TelaPrincipal.class);
-        startActivity(intent);
-    }
+        String nome = edtNome.getText().toString();
+        Intent intentEnvia = new Intent(getApplicationContext(),TelaPrincipal.class);
+        Bundle parametros = new Bundle();
+        parametros.putString("chave_nome",nome);
+        intentEnvia.putExtras(parametros);
+        startActivity(intentEnvia);
 
+        if(edtNome==edtSenha) {
+            Intent intent = new Intent(this, TelaPrincipal.class);
+            startActivity(intent);
+        }
+
+        if(edtNome != edtSenha)
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "Nome e senha informados são diferente";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
 }

315da85617687b3294ee32c52b770881be161513
FelicitySmoak1101 <42353284+FelicitySmoak1101@users.noreply.github.com>
2021-03-17 00:03:42 -0300

    Update TelaPrincipal.java

--- TelaPrincipal.java
+++ TelaPrincipal.java
@@ -1,37 +1,50 @@
 package com.example.aula;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView edtCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
 
+        edtCliente=(TextView) findViewById(R.id.cliente);
+        Intent intentRecebe = getIntent();
+        Bundle parametros = intentRecebe.getExtras();
+        if (parametros != null){
+            String nome = parametros.getString("chave_nome");
+            edtCliente = findViewById(R.id.editTextTextPersonName);
+        }
+
+
         Button buttonSplash = (Button) findViewById(R.id.btnSplash);
-        buttonSplash.setOnClickListener(this);
+        buttonSplash.setOnClickListener(this::onClickSplash);
 
         Button buttonVoltar = (Button) findViewById(R.id.btnVoltar);
-        buttonVoltar.setOnClickListener(this);
+        buttonVoltar.setOnClickListener(this::onClickVoltar);
     }
 
 
-    @Override
     public void onClickSplash(View v) {
         Intent inte = new Intent(this, Splash.class);
         startActivity(inte);
     }
+    
+    public void onClickVoltar(View v) {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+    }
+}
 
     @Override
     public void onClickVoltar(View v) {
